b71919b4fe427b9393c4e4557aa72621218557d1
JoelMorel <jamkding@gmail.com>
2019-03-27 17:15:02 -0400

    attempt to fix mongoURI

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://dbadmin:scooby69@projot-prod-gwff0.mongodb.net/test?retryWrites=true"
+      "mongodb://dbadmin:scooby69@projot-prod-gwff0.mongodb.net/test?retryWrites=true"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/projot-dev" };
